509f78455558c82ffd60963279aae4675776100b
siangyeh8818 <siangyeh@gmail.com>
2018-10-08 15:07:45 +0800

    fix python bug

--- image-Dockerfile/run.sh
+++ image-Dockerfile/run.sh
@@ -1,4 +1,10 @@
+#!/bin/bash
+env | grep ES_SOURCE_IP >> env.txt
+env | grep ES_SOURCE_PORT >> env.txt
+env | grep LOG_EFFECTIVE_MONTH >> env.txt
+env | grep ES_MANAGEMENT_LOG_LEVEL >> env.txt
+
 crond  
-#touch /var/log/cron.log  
+touch /var/log/cron.log  
 #python dairy.py 
 sleep infinity
